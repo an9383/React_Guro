e87a9b2de4196140bd32d63eea4680a78ff762af
an9383 <an9383@gmail.com>
2025-05-20 13:15:32 +0900

    all

--- 4. dashboard-horizon_an9383/src/theme/index.js
+++
@@ -1,6 +0,0 @@
-import { extendTheme } from '@chakra-ui/react'
-import { styles } from './styles'
-import { buttonStyles } from './components/button'
-import { breakpoints } from './foundations/breakpoints'
-
-export default extendTheme(styles, buttonStyles, { breakpoints })
